72fddedf573af3eb705db7d075c8f08cdbf0d556
administrator <admin@uavbomb.cn>
2025-02-09 21:25:20 +0800

    Fix bugs.

--- vikit_ros/include/vikit/params_helper.h
+++ vikit_ros/include/vikit/params_helper.h
@@ -23,7 +23,7 @@ bool hasParam(const rclcpp::Node::SharedPtr &nh, const std::string& name)
   return nh->has_parameter(name);
 }
 
-template<typename T>
+/* template<typename T>
 T getParam(const rclcpp::Node::SharedPtr &nh, const std::string& name, const T& defaultValue)
 {
   T v;
@@ -37,9 +37,45 @@ T getParam(const rclcpp::Node::SharedPtr &nh, const std::string& name, const T& 
     RCLCPP_WARN(nh->get_logger(), "Cannot find value for parameter: %s, assigning default: %s", name.c_str(), std::to_string(defaultValue).c_str());
 	  nh->declare_parameter(name, defaultValue);
   return defaultValue;
-}
+} */
 
 template<typename T>
+T getParam(const rclcpp::Node::SharedPtr &nh, const std::string& name, const T& defaultValue)
+{
+  T v;
+  if(nh->has_parameter(name))
+  {
+    v = nh->get_parameter(name).get_value<T>();
+    RCLCPP_INFO(nh->get_logger(), "Found parameter: %s, value: %s", name.c_str(), std::to_string(v).c_str());
+    return v;
+  }
+  else
+  {
+    RCLCPP_WARN(nh->get_logger(), "Cannot find value for parameter: %s, assigning default: %s", name.c_str(), std::to_string(defaultValue).c_str());
+    nh->declare_parameter(name, defaultValue);
+  }
+  return defaultValue;
+}
+  
+template<>
+std::string getParam<std::string>(const rclcpp::Node::SharedPtr &nh, const std::string& name, const std::string& defaultValue)
+{
+  std::string v;
+  if(nh->has_parameter(name))
+  {
+    v = nh->get_parameter(name).get_value<std::string>();
+    RCLCPP_INFO(nh->get_logger(), "Found parameter: %s, value: %s", name.c_str(), v.c_str());
+    return v;
+  }
+  else
+  {
+    RCLCPP_WARN(nh->get_logger(), "Cannot find value for parameter: %s, assigning default: %s", name.c_str(), defaultValue.c_str());
+    nh->declare_parameter(name, defaultValue);
+  }
+  return defaultValue;
+}
+
+/* template<typename T>
 T getParam(const rclcpp::Node::SharedPtr &nh, const std::string& name)
 {
   T v;
@@ -51,6 +87,39 @@ T getParam(const rclcpp::Node::SharedPtr &nh, const std::string& name)
   }
   
   RCLCPP_INFO(nh->get_logger(), "Found parameter: %s, value: %s", name.c_str(), std::to_string(v).c_str());
+  return v;
+} */
+
+	 
+template<typename T>
+typename std::enable_if<!std::is_same<T, std::string>::value, T>::type
+getParam(const rclcpp::Node::SharedPtr &nh, const std::string& name)
+{
+  T v;
+  int i = 0;
+  while(!nh->get_parameter(name, v))
+  {
+    RCLCPP_ERROR(nh->get_logger(), "Cannot find value for parameter: %s, will try again.", name.c_str());
+    if ((i++) >= 5) return T();
+  }
+  
+  RCLCPP_INFO(nh->get_logger(), "Found parameter: %s, value: %s", name.c_str(), std::to_string(v).c_str());
+  return v;
+}
+  
+template<typename T>
+typename std::enable_if<std::is_same<T, std::string>::value, T>::type
+getParam(const rclcpp::Node::SharedPtr &nh, const std::string& name)
+{
+  T v;
+  int i = 0;
+  while(!nh->get_parameter(name, v))
+  {
+    RCLCPP_ERROR(nh->get_logger(), "Cannot find value for parameter: %s, will try again.", name.c_str());
+    if ((i++) >= 5) return T();
+  }
+  
+  RCLCPP_INFO(nh->get_logger(), "Found parameter: %s, value: %s", name.c_str(), v.c_str());
   return v;
 }
 
